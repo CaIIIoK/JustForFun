7b0be59a177fad8bc6ac87de8d594051a666689d
Oleksandr Rudyi <calllok.rud@gmail.com>
2016-01-14 23:03:17 +0200

    Second changes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Alex on 14.01.2016.
  */
@@ -5,5 +9,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("First");
+
+
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 10 ; i++) {
+            arr.add(i);
+        }
+
+        for(Integer a : arr){
+            System.out.println("Value: " + a);
+        }
     }
 }
